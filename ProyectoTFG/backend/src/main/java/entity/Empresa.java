30cf5a22fc0e098aac159e9705bf20b3c07be374
Jose Maria villafañe Marcos <infjmv00@estudiantes.unileon.es>
2026-03-09 13:35:13 +0100

    Añadidos nombres de las columnas

--- ProyectoTFG/backend/src/main/java/entity/Empresa.java
+++ ProyectoTFG/backend/src/main/java/entity/Empresa.java
@@ -1,9 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * https://www.youtube.com/watch?v=KuM6OtuaYRs&list=PLGRDMO4rOGcNLnW1L2vgsExTBg-VPoZHr
  */
 
 package entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
@@ -34,12 +36,18 @@ public class Empresa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name="CIF_Empresa")
     private String Cif_Empresa;
+     @Column (name="nombre_empresa")
     private String nombre_empresa;
+     @Column (name="direccion_empresa")
     private String direccion_empresa;
+     @Column (name="Telefonoprov")
     private Integer telefono_empresa;
+     @Column (name="e_mail", nullable=false, unique=true )
     private String correo_electronico;
-    private boolean activo;
-    private String prueba;
+     @Column (name="activoEmpresa")
+     private boolean activo;
+  
 
 }
