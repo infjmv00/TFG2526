ad4fb051178046bb7a389ba516d5fb2c7a9229a3
jmvm_ <jmvm_@10.130.93.226>
2026-05-25 16:53:56 +0200

    Añadiendo CrossOriginenEmpresaController

--- ProyectoTFG/backend/src/main/java/controller/EmpresaController.java
+++ ProyectoTFG/backend/src/main/java/controller/EmpresaController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import service.EmpresaService;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/empresas")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmpresaController {
     
     // Construccion de REST API para añadir una empresa
